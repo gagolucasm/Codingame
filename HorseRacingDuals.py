de814f96cbd08188cd26d1fe26870887ed52fd32
Lucas Gago <gagolucasm@hotmail.com>
2016-11-18 17:23:57 +0100

    Create HorseRacingDuals.py

---
+++ HorseRacingDuals.py
@@ -0,0 +1,16 @@
+import bisect, sys
+
+N = int(input())
+horses = []
+mini, diff = sys.maxsize, None
+
+for i in range(N):
+    bisect.insort(horses, int(input()))
+
+for i in range(N-1):
+    diff = horses[i+1] - horses[i]
+    if diff < mini:
+        mini = diff
+
+print(mini)
+            
